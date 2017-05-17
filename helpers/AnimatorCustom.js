e9e5ad439351ec17f0a2be879053cb06344ebdcb
Andrea Mazzilli <andreamazzilli@Andrea-MacBook-Pro.local>
2017-05-17 10:45:11 +0200

    android crash bug fixed

--- helpers/AnimatorCustom.js
+++ helpers/AnimatorCustom.js
@@ -12,27 +12,31 @@ const _cycleAnimation = function( payload ) {
 }
 
 const _buildSingleAnimation = function ( payload ) {
-  if ( typeof payload.x == 'number' ) return Animated.timing( this.state.animation[ 'translateX' ] , { useNativeDriver : true , toValue : payload.x , duration : payload.t } )  ;
-  if ( typeof payload.y == 'number' ) return Animated.timing( this.state.animation[ 'translateY' ] , { useNativeDriver : true , toValue : payload.y , duration : payload.t } )  ;
-  if ( typeof payload.s == 'number' ) return Animated.timing( this.state.animation[ 'scale' ] , { useNativeDriver : true , toValue : payload.s , duration : payload.t } )  ;
-  if ( typeof payload.r == 'number' ) return Animated.timing( this.state.animation[ 'rotate' ] , { useNativeDriver : true , toValue : payload.r , duration : payload.t } )  ;
-  if ( typeof payload.o == 'number' ) return Animated.timing( this.state.animation[ 'opacity' ] , { useNativeDriver : true , toValue : payload.o , duration : payload.t } )  ;
+  var duration = payload.t * 1000 ;
+  var type = 'timing' ;
+  if ( typeof payload.f == 'number' ) type = 'spring' ;
+  if ( typeof payload.x == 'number' ) return Animated[ type ]( this.state.animation[ 'translateX' ] , { useNativeDriver : true , toValue : payload.x , duration : duration , friction : payload.f } )  ;
+  if ( typeof payload.y == 'number' ) return Animated[ type ]( this.state.animation[ 'translateY' ] , { useNativeDriver : true , toValue : payload.y , duration : duration , friction : payload.f } )  ;
+  if ( typeof payload.s == 'number' ) return Animated[ type ]( this.state.animation[ 'scale' ] , { useNativeDriver : true , toValue : payload.s , duration : duration , friction : payload.f } )  ;
+  if ( typeof payload.r == 'number' ) return Animated[ type ]( this.state.animation[ 'rotate' ] , { useNativeDriver : true , toValue : payload.r , duration : duration , friction : payload.f } )  ;
+  if ( typeof payload.o == 'number' ) return Animated[ type ]( this.state.animation[ 'opacity' ] , { useNativeDriver : true , toValue : payload.o , duration : duration , friction : payload.f } )  ;
 }
 
 const _buildParallelAnimation = function( payload ) {
   var step = [] ;
-  if ( typeof payload.x == 'number' ) step.push( _buildSingleAnimation.call( this , { x : payload.x , t : payload.t } ) ) ;
-  if ( typeof payload.y == 'number' ) step.push( _buildSingleAnimation.call( this , { y : payload.y , t : payload.t } ) ) ;
-  if ( typeof payload.s == 'number' ) step.push( _buildSingleAnimation.call( this , { s : payload.s , t : payload.t } ) ) ;
-  if ( typeof payload.r == 'number' ) step.push( _buildSingleAnimation.call( this , { r : payload.r , t : payload.t } ) ) ;
-  if ( typeof payload.o == 'number' ) step.push( _buildSingleAnimation.call( this , { o : payload.o , t : payload.t } ) ) ;
+  if ( typeof payload.x == 'number' ) step.push( _buildSingleAnimation.call( this , { x : payload.x , t : payload.t , f : payload.f } ) ) ;
+  if ( typeof payload.y == 'number' ) step.push( _buildSingleAnimation.call( this , { y : payload.y , t : payload.t , f : payload.f } ) ) ;
+  if ( typeof payload.s == 'number' ) step.push( _buildSingleAnimation.call( this , { s : payload.s , t : payload.t , f : payload.f } ) ) ;
+  if ( typeof payload.r == 'number' ) step.push( _buildSingleAnimation.call( this , { r : payload.r , t : payload.t , f : payload.f } ) ) ;
+  if ( typeof payload.o == 'number' ) step.push( _buildSingleAnimation.call( this , { o : payload.o , t : payload.t , f : payload.f } ) ) ;
   return Animated.parallel( step );
 }
 
 const AnimatorCustom = function( payload ) {
   var me = this ;
   var steps = [] ;
-  const { anim , callback } = payload ;
+  var anim = payload.anim ;
+  const { callback } = payload ;
 
   var animation = {
     translateX : new Animated.Value( 0 )
@@ -46,7 +50,13 @@ const AnimatorCustom = function( payload ) {
     this.setState( { animation : animation } ) ;
     var seqs = [] ;
     for( let x = 0 ; x < anim.length ; ++x ){
-      seqs.push( _buildParallelAnimation.call( this , { y : anim[ x ].y , x : anim[ x ].x , t : anim[ x ].t , r : anim[ x ].r , s : anim[ x ].s , o : anim[ x ].o } ) )
+      if ( !anim[ x ].x ) anim[ x ].x = 0 ;
+      if ( !anim[ x ].y ) anim[ x ].y = 0 ;
+      if ( !anim[ x ].s ) anim[ x ].s = 1 ;
+      if ( !anim[ x ].o ) anim[ x ].o = 1 ;
+      if ( !anim[ x ].r ) anim[ x ].r = 0 ;
+
+      seqs.push( _buildParallelAnimation.call( this , { y : anim[ x ].y , x : anim[ x ].x , t : anim[ x ].t , r : anim[ x ].r , s : anim[ x ].s , o : anim[ x ].o , f : anim[ x ].f } ) )
     }
     Animated.sequence( seqs ).start() ;
 
